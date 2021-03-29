58ece37a88b2b124b04545b99a9c6f834bfbfe1c
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2021-03-29 10:59:25 +0200

    påske update

--- portfolio-react/src/components/Index.js
+++ portfolio-react/src/components/Index.js
@@ -9,14 +9,36 @@ const Index = () =>{
     return(
         <div className="app">
             <img className="bannerImg" src={BannerImg}/>
+{/* ============ SKILLS =========== */}
             <div id="skills">
                 <div className="col-lg-6">
                     <img className="cartoonMe pt-4" src={CartoonMe}/>
                 </div>
                 <div className="col-lg-6"></div>
-                <div className="col-sm-12 skillImages">
-                    {ImagePool.map((imgSrc, index)=> (<img src={imgSrc} key={index}/>))}
+                <div className="col-sm-12 row skillImage">
+                    {ImagePool.map((img, index)=> (
+                        <div className="col-md-2 pl-5">
+                            <img src={img.src} key={index} className={"skillImage"+index}/>
+                            <p>{img.text}</p>
+                        </div>      
+                    ))}
                 </div>
+            </div>
+{/* ================= PROJECTS =================== */}
+            <div className="projects row">
+                <div className="project1 col-md-6">
+                    <div className="projectImage1"></div>
+                </div>
+                <div className="project2 col-md-6">
+                    <div className="projectImage2"></div>
+                </div>
+                {/* <div className="project3 col-md-6">
+                    <div className="projectImage3"></div>
+                </div> */}
+            </div>
+{/* ================= FOOTER =================== */}
+            <div>
+                
             </div>
             <h1>Index</h1>
             <div className="testelement">
--- portfolio-react/src/context/ImagePool.js
+++ portfolio-react/src/context/ImagePool.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import JavaScriptIcon from '.././assets/images/SkillIcons/javascript.png';
 import NodeJSIcon from '.././assets/images/SkillIcons/nodejs2.png';
+import ReactIcon from '.././assets/images/SkillIcons/reactIcon.png';
+import Vue from '.././assets/images/SkillIcons/vue.png';
+import Spring from '.././assets/images/SkillIcons/spring.png';
+import Thymeleaf from '.././assets/images/SkillIcons/thymeleaf.png';
+import Java from '.././assets/images/SkillIcons/java.png';
+import Linux from '.././assets/images/SkillIcons/linux.png';
+import Mysql from '.././assets/images/SkillIcons/mysql.png';
+import GitIcon from '.././assets/images/SkillIcons/Git-Icon.png';
+import Illustrator from '.././assets/images/SkillIcons/illustrator.png';
+import photoshop from '.././assets/images/SkillIcons/photoshop.png';
 
 const ImagePool = [
-    JavaScriptIcon, NodeJSIcon
+    {src : JavaScriptIcon, text : "test"},
+    {src : NodeJSIcon, text : "test2"},
+    {src : ReactIcon, text : "test"},
+    {src : Vue, text : "test"},
+    {src : Spring, text : "test"},
+    {src : Thymeleaf, text : "test"},
+    {src : Java, text : "test"},
+    {src : Linux, text : "test"},
+    {src : Mysql, text : "test"},
+    {src : GitIcon, text : "test"},
+    {src : Illustrator, text : "test"},
+    {src : photoshop, text : "test"},
+
+
+    // JavaScriptIcon, NodeJSIcon, ReactIcon, Vue, Spring, Thymeleaf, Java, Mysql
 ]
 
 export default ImagePool;
